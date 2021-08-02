0a87f4b97cc0728ea398b22859221dade6b2b14d
gkswn911 <gkswn911@naver.com>
2021-08-02 22:07:44 +0900

    ADD : Lec4 Exercise tryCatchExercise.cs

---
+++ Lec4/tryCatchExercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3 };
+
+            try
+            {
+                array[3] = 10;
+            }
+            catch ( IndexOutOfRangeException e)
+            {
+                Console.WriteLine("배열 인덱스 에러 발생");
+                Console.WriteLine(e.ToString());
+                array[2] = 10;
+            }
+            finally
+            {
+                for (int i = 0; i < array.Length; i++)
+                    Console.Write("{0} ", array[i]);
+            }
+        }
+    }
+}
